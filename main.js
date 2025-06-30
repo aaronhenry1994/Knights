c902cff03d5d50bf5c3ab56dc940e3bed2873f9b
Aaron Henry <aaronhenry1994@outlook.com>
2025-06-30 08:14:16 -0600

    Created a function to make a knight make a random move on a chessboard.

--- main.js
+++ main.js
@@ -21,16 +21,15 @@ class legalMoves {
             [x - 2, y + 1],
             [x - 2, y - 1]
         ];
-    };
-};
-
-class borders {
-    constructor(x, y) {
-        this.outOfBounds = [
+        this.borders = [
             [8, y],
             [x, 8],
             [-1, y],
             [x, -1]
+        ];
+        this.goal = [
+            [7, y],
+            [x, 7]
         ];
     };
 };
@@ -46,7 +45,15 @@ function calculateMoves(x, y) {
     let knight = new Node(x, y);
     console.log(knight);
 
+    let turn = new legalMoves(knight.x, knight.y);
 
+    console.log(turn.moves);
+
+    const randomMove = (array) => array[Math.floor(Math.random() * turn.moves.length)];
+    let final = randomMove(turn.moves);
+
+    return final;
 };
 
 calculateMoves(0, 0);
+console.log(calculateMoves(0, 0))
